3bd8d1de24e10ae4d94bbc17b89138a5b2ac476b
madmedmod <madmedmod@gmail.com>
2019-06-16 02:19:49 +0700

    commiting all finished single file component courses

---
+++ src/components/BigYellowUsername.vue
@@ -0,0 +1,23 @@
+<template>
+    <p class="username">{{ username }}</p>
+</template>
+
+<script>
+export default {
+    props: {
+        username: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    $size: 50pt;
+    $yellow: #DCAE1D;
+    .username {
+        font-size: $size;
+        color: $yellow;
+    }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="showUsername = !showUsername">Toggle Username</button>
+    <div v-if="showUsername">
+      <BigYellowUsername username="madmedmod"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      showUsername: false
+    }
+  },
+  components: {
+    BigYellowUsername: () => import (/* webpackChunkName: "BYU" */'./BigYellowUsername.vue')
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
